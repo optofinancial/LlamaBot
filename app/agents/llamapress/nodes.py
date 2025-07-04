387727ea57148dce1451def0c092935efa5652cb
Kody Kendall <kodyckendall@gmail.com>
2025-07-04 12:19:32 -0700

    WIP: Creating ability to write HTML page back to LlamaPress

--- app/agents/llamapress/nodes.py
+++ app/agents/llamapress/nodes.py
@@ -21,14 +21,25 @@ class LlamaPressState(MessagesState):
     agent_instructions: str
 
 # Tools
+@tool
+def write_html_page(full_html_document: str, message_to_user: str, internal_thoughts: str, state: Annotated[LlamaPressState, InjectedState]) -> str:
+   """
+   Write an HTML page to the filesystem.
+   full_html_document is the full HTML document to write to the filesystem, including CSS and JavaScript.
+   message_to_user is a string to tell the user what you're doing.
+   internal_thoughts are your thoughts about the command.
+   state is the state of the agent.
+   """
+   print ("API TOKEN", state.get("api_token")) # empty. only messages is getting passed through.
+
+   print("Write to filesystem!")
+   return "HTML page written to filesystem!"
 
 # Global tools list
-# tools = [run_rails_console_command]
-tools = []
+tools = [write_html_page]
 
 # Node
 def llamapress(state: LlamaPressState):
-   
    additional_instructions = state.get("agent_instructions")
 
    # System message
@@ -60,4 +71,112 @@ def build_workflow(checkpointer=None):
 
     react_graph = builder.compile(checkpointer=checkpointer)
 
-    return react_graph
+    return react_graph   
+
+# Tools
+# @tool
+# def run_rails_console_command(rails_console_command: str, message_to_user: str, internal_thoughts: str, state: Annotated[LlamaBotState, InjectedState]) -> str:
+
+# eventually make this a class 
+# make it reusable/configurable to many workflows
+# async def build_workflow_saas(context: WebSocketRequestContext):
+#     workflow = StateGraph(LlamaPressWorkflowState)
+    
+#     #router setup
+#     workflow.set_entry_point("router")
+#     workflow.add_node("router", partial(route_request, context=context))
+    
+#     #router condition
+#     workflow.add_conditional_edges(
+#         "router",
+#          lambda x: x["next"], 
+#          {
+#              "respond_naturally": "respond_naturally",
+#              "coding_agent": "coding_agent_saas",
+#              "end": "end"
+#          }
+#     )
+
+#     workflow.add_node("end", partial(end_workflow, context=context))
+#     workflow.add_node("coding_agent_saas", partial(coding_agent_saas, context=context))
+#     workflow.add_node("respond_naturally", partial(respond_naturally, context=context))
+#     workflow.add_node("remove_element", partial(remove_element, context=context))
+#     workflow.add_node("improve_design", partial(improve_design, context=context))
+#     workflow.add_node("clone_webpage", partial(clone, context=context))
+
+#     # Add end as a valid endpoint
+#     workflow.set_finish_point("end")
+
+#     workflow.add_conditional_edges(
+#         "coding_agent_saas",
+#          lambda x: x["next"], 
+#          {
+#             "remove_element" : "remove_element",
+#             "improve_design" : "improve_design",
+#             "clone_router_agent" : "clone_router_agent",
+#          }
+#     )
+
+#     workflow.add_node("clone_router_agent", partial(clone_router_agent, context=context))
+
+#     workflow.add_conditional_edges(
+#         "clone_router_agent",
+#          lambda x: x["next"], 
+#          {
+#             "deep_vision_agent" : "deep_vision_agent",
+#             "clone_webpage" : "clone_webpage",
+#             "brand_clone_agent" : "brand_clone_agent"
+#          }
+#     )
+
+#     workflow.add_node("deep_vision_agent", partial(deep_vision_agent, context=context))
+#     workflow.add_edge("deep_vision_agent", "deep_clone_agent")
+
+#     workflow.add_node("deep_clone_agent", partial(deep_clone_agent, context=context))
+#     workflow.add_edge("deep_clone_agent", "end")
+
+#     workflow.add_node("brand_clone_agent", partial(brand_clone_agent, context=context))
+#     workflow.add_edge("brand_clone_agent", "end")
+
+#     workflow.add_edge("respond_naturally", "end")
+#     workflow.add_edge("remove_element", "end")
+#     workflow.add_edge("clone_webpage", "end") #end after clone
+
+#     workflow.add_conditional_edges(
+#         "improve_design",
+#          lambda x: x["next"], 
+#          {
+#             "improve_selected_element" : "improve_selected_element",
+#             "design_requirements_agent" : "design_requirements_agent",
+#             "debug_agent" : "debug_agent"
+#          }
+#     )
+
+#     workflow.add_node("improve_selected_element", partial(improve_selected_element, context=context))
+#     workflow.add_edge("improve_selected_element", "end")
+    
+#     workflow.add_node("design_requirements_agent", partial(design_requirements_agent, context=context))
+#     workflow.add_edge("design_requirements_agent", "make_wholesale_change")
+
+#     workflow.add_node("make_wholesale_change", partial(make_wholesale_change, context=context))
+#     workflow.add_edge("make_wholesale_change", "end")
+
+#     workflow.add_node("debug_agent", partial(debug_agent, context=context))
+#     workflow.add_edge("debug_agent", "multiple_fragments")
+
+#     workflow.add_node("multiple_fragments", partial(multiple_fragments, context=context))
+#     workflow.add_edge("multiple_fragments", "end")
+
+#     # We have to do this workaround, because otherwise Langgraph Studio won't compile and it will complain that 
+#     # a checkpointer is already defined when running langgraph dev. (They set up their own checkpointer when running langgraph dev)
+#     # I think this is because LangChain wants to incentivize people to use their own checkpointer
+#     # that's set up when hosting their own LangGraph server.
+#     # But we're not using their hosted LangGraph server, and we want to debug using langgraph dev, so we have do this workaround.
+#     checkpointer = None
+#     if type(context) != dict: # context will be a dict when we're running langgraph dev, but if we're running fastapi, context will be a WebSocketRequestContext object
+#         checkpointer = context.langgraph_checkpointer
+    
+#     # It's confusing why context isn't passed as a parameter to the workflow.compile function.
+#     # And it's because we're already passing it into each node using the partial function.
+#     app = workflow.compile(checkpointer=checkpointer, debug=True)
+#     return app
